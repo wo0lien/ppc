5881e2771a5eec55c8704bdf6a7eaf8019ce9b77
noe <nonogerboy@gmail.com>
2020-01-07 15:46:52 +0100

    commit

--- card.py
+++ card.py
@@ -1,7 +1,8 @@
 # -*-coding: utf8-*-
 
 class GameCard:
-
     def __init__(self, color, nb):
         self.color = color
         self.nb = nb
+    def __str__(self):
+        str("||"+self.color+"|"+self.nb+"||")
--- main.py
+++ main.py
@@ -17,29 +17,7 @@ defausse = list()
 playLock = Lock()
 defausseLock = Lock()
 
-def displayer(display_queue, display_event, response_event):
-    """
-    Thread qui sera appelé par le process joueur pour gérer les affichages.
-    """
-    while True:
-        display_event.wait()
-        value = display_queue.get()
-
-        print(value) # ensuite on rajoutera le traitement et le socket la dessus
-
-        display_event.clear()
-        response_event.set()
-
-def player(key, deck, event):
-
-    # lien avec le thread displayer
-
-    display_queue = queue.Queue()
-    display_event = threading.Event()
-    response_event = threading.Event()
-
-    displayer = threading.Thread(target="displayer", args=(display_queue, display_event, response_event))
-    
+def player(key, deck, event):    
     # creation d'une variable globale
 
     global playLock, defausseLock
@@ -48,7 +26,7 @@ def player(key, deck, event):
     # on recupere la defausse
 
     with defausseLock:
-        ldefause = defausse
+        ldefausse = defausse
 
     mq = sysv_ipc.MessageQueue(key)
 
@@ -57,16 +35,19 @@ def player(key, deck, event):
             # wait une action du joueur OU une action sur le board avec les event
 
             # handle une modification de la liste
-            if event.isSet():
+            if event.is_set():
                 with defausseLock():
-                    ldefause = defausse
+                    ldefausse = defausse
                 event.clear()
 
             # if ordre de jouer une carte:
             #   cardindex = cardsaisie
             #   break
-
-        cardIndex = 0
+        print("Defausse :"+ldefausse)
+        for i in range(len(deck)):
+            print(deck[i])
+            
+        cardIndex = int(input())
         # envoie la carte et attend une reponse
 
         playLock.acquire()
@@ -90,7 +71,7 @@ if __name__ == "__main__":
     mq = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
 
     # initialisation
-    nbJoueurs = 2
+    nbJoueurs = 1
     lcarte=list()
     for i in range(10):
         lcarte.append(GameCard("r",i+1))
@@ -98,11 +79,15 @@ if __name__ == "__main__":
 
     for i in range(nbJoueurs):
         playerDeck=list()
-        for i in range(10):
-            playerDeck.append(lcarte.pop(random.randint(0, len(lcarte))))
+        for i in range(5):
+            playerDeck.append(lcarte.pop(random.randint(0, len(lcarte)-1)))
         ev = Event()
         p = Process(target=player, args=(key, playerDeck, ev,))
         p.start()
+    for i in range(len(lcarte)):
+        pioche.append(lcarte.pop(random.randint(0, len(lcarte)-1)))
+    
+    defausse.append(pioche.pop())
 
     # rentre dans le jeu
 
@@ -113,7 +98,7 @@ if __name__ == "__main__":
         value = message.decode()
         # Si c'est valide ou non on renvoie dans la queue le nombre de cartes a piocher
         try:
-            carteR = GameCard(value)
+            carteR = GameCard(value,1)
         except TypeError:
             print("NotACard")
         else:
