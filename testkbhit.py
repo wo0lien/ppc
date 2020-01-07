156d16d6efdf039eaf41990efc47a75ef43a79a4
Tom Sampic <tom.sampic@insa-lyon.fr>
2020-01-07 16:10:22 +0100

    test kbhit

---
+++ testkbhit.py
@@ -0,0 +1,29 @@
+from kbhitmod import KBHit
+import os
+
+# Windows
+if os.name == 'nt':
+    import msvcrt
+
+# Posix (Linux, OS X)
+else:
+    import sys
+    import termios
+    import atexit
+    from select import select
+
+if __name__ == "__main__":
+    
+    kb = KBHit()
+
+    print('Hit any key, or ESC to exit')
+
+    while True:
+
+        if kb.kbhit():
+            c = kb.getch()
+            if ord(c) == 27: # ESC
+                break
+            print(c)
+
+    kb.set_normal_term()
