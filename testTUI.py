d619dd7169e3f7842c5ad96dd181450942e3882c
Tom Sampic <tom.sampic@insa-lyon.fr>
2020-01-15 16:27:38 +0100

    up

--- testTUI.py
+++ testTUI.py
@@ -1,18 +1,56 @@
-#!/usr/bin/env python3
-import npyscreen
+from asciimatics.widgets import Frame, ListBox, Layout, Divider, Text, \
+    Button, TextBox, Widget
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+from asciimatics.exceptions import ResizeScreenError, NextScene, StopApplication
+import sys
 
-class App(npyscreen.StandardApp):
-    def onStart(self):
-        self.addForm("MAIN", MainForm, name="Player UI")
+class ListView(Frame):
+    def __init__(self, screen):
+        super(ListView, self).__init__(screen,
+                                       screen.height * 2 // 3,
+                                       screen.width * 2 // 3,
+                                       on_load=self._reload_list,
+                                       hover_focus=True,
+                                       can_scroll=False,
+                                       title="Deck")
 
-class DeckBox(npyscreen.BoxTitle):
-    _contained_widget = npyscreen.MultiLineEdit
+        # Create the form for displaying the list of contacts.
+        self._list_view = ListBox(
+            Widget.FILL_FRAME,[("first option",1),("Second option", 2)],
+            name="test",
+            on_select=self._edit)
+        layout = Layout([100], fill_frame=True)
+        self.add_layout(layout)
+        layout.add_widget(self._list_view)
+        self.fix()
+        self._on_pick()
 
-class MainForm(npyscreen.Form):
-    def create(self):
-        # self.title = self.add(npyscreen.TitleText, name="Deck", value="")
-        self.deck = self.add(DeckBox, footer="Not editable", editable=False, )
-        self.deck.value="coucou noe"
+    def _on_pick(self):
+        pass
 
-MyApp = App()
-MyApp.run()
+    def _reload_list(self, new_value=None):
+        pass
+
+    def _add(self):
+        pass
+
+    def _edit(self):
+        pass
+
+    def _delete(self):
+        pass
+
+    @staticmethod
+    def _quit():
+        raise StopApplication("User pressed quit")
+
+def demo(screen):
+    scenes = [
+        Scene([ListView(screen)], -1, name="Main"),
+    ]
+
+    screen.play(scenes, stop_on_resize=True, start_scene=scenes[0], allow_int=True)
+
+Screen.wrapper(demo, catch_interrupt=True)
+sys.exit(0)
