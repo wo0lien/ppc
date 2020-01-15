36dcdae39b32783b586c449050b42c330d8a089e
Tom Sampic <tom.sampic@insa-lyon.fr>
2020-01-15 17:47:26 +0100

    fix diaplay bugs

--- uptodate/displayer.py
+++ uptodate/displayer.py
@@ -9,6 +9,7 @@ class bcolors:
     BLACK = '\u001b[30m'
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
+    CYAN = '\u001b[36m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
@@ -43,21 +44,41 @@ def displayer(dqueue):
 
         os.system('clear')
 
-        print(bcolors.WARNING)
+        if (defausse.color=="r"):
+            print(bcolors.WARNING)
+        else:
+            print(bcolors.CYAN)
+        
         tprint(str(defausse.nb))
+        
+
         print(bcolors.FAIL)
         redsum = ""
+        redindexes = ""
+        i = 0
+        for card in cards:
+            if (card.color=="r"):
+                redindexes += "        " + str(i)
+            i += 1
         for card in reds:
             redsum += "   " + str(card.nb)
         if (redsum != ""):
+            print(redindexes)
             tprint(redsum)
 
         print(bcolors.OKBLUE)
         bluesum = ""
+        blueindexes = ""
+        i = 0
+        for card in cards:
+            if (card.color=="b"):
+                blueindexes += "       " + str(i)
+            i += 1
         for card in blues:
             bluesum += "   " + str(card.nb)
         if (bluesum != ""):
             tprint(bluesum)
+            print(blueindexes)
 
         print(bcolors.BLACK)
 
