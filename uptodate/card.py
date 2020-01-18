bea73eca57dec7e9387adead299f4a28605204a8
Tom Sampic <tom.sampic@insa-lyon.fr>
2020-01-18 08:34:04 +0100

    formatting gamecard and adding comments

--- uptodate/card.py
+++ uptodate/card.py
@@ -1,12 +1,25 @@
 # -*-coding: utf8-*-
+"""
+La classe gamecard permet le transport facilité des données entre les threads client et displayer
+"""
+
 
 class GameCard:
+    """
+    Constructeur iinitialisant une nouvelle carte a partir de 2 parametres
+    Parameters:
+        color (string): couleur de la carte r pour red ou b pour blue
+        nb (int): valeur de la carte
+    """
+
     def __init__(self, color, nb):
         self.color = str(color)
         self.nb = int(nb)
-    
+
     def __str__(self):
+        """Affichage d'une carte - utilisé pour le debug"""
         return ("||"+str(self.color)+"|"+str(self.nb)+"||")
-    
+
     def tosend(self):
+        """Fonction qui genere un code string pour la communication avec le board"""
         return str(self.color)+"|"+str(self.nb)
